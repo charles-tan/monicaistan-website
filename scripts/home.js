84d8a2d24a64d42821b08d3adaa82ebc2b7b3b74
Conversable Air <charles@conversable.com>
2017-07-02 18:53:44 -0500

    added rest of html fields for homepage and json file for projects

--- scripts/home.js
+++ scripts/home.js
@@ -1,22 +1,46 @@
 var page = 1;
 
 $(document).ready(function() {
-    $('#nav_list').css({'visibility':'hidden'});
+    // $('#nav_list').css({'visibility':'hidden'});
+    var projects;
+    $.getJSON("projects.json", function(data) {
+        projects = data;
+        var lists = [];
+        var listlen = 3;
+        for(var i = 0; i < listlen; i++) {
+            lists.push([]);
+        }
+        for(var i = 0; i < projects.length; i++) {
+            lists[i % listlen].push(projects[i]);
+        }
+        for(var i = 0; i < listlen; i++) {
+            var list = lists[i]
+            const markup = `
+            <ul class="project_list">
+                ${list.map(p =>
+                        `<li><img src="${p.cover_img}" height=400 width=400></li>`
+                 ).join('')}
+             </ul>
+            `;
+            $('#work_lists').append(markup);
+        }
+    })
+
+    // function showPage() {
+    //     switch page {
+    //     case 3:
+    //         code block
+    //         break;
+    //     case 2:
+    //         code block
+    //         break;
+    //     default:
+    //         code block
+    //     }
+    // }
+
+    function hamburgerMenu(x) {
+        x.classList.toggle("change");
+    }
+
 })
-
-// function showPage() {
-//     switch page {
-//     case 3:
-//         code block
-//         break;
-//     case 2:
-//         code block
-//         break;
-//     default:
-//         code block
-//     }
-// }
-
-function hamburgerMenu(x) {
-    x.classList.toggle("change");
-}
